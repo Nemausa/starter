977d418fc73c49daca38f57c2863e11a6623461b
Nemausa <tappanmorris@outlook.com>
2024-09-11 22:35:54 +0800

    feat(screenshot): Add code screenshot tool
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,4 +47,11 @@ return {
       }
     end,
   },
+  {
+    "ellisonleao/carbon-now.nvim",
+    lazy = true,
+    cmd = "CarbonNow",
+    ---@param opts cn.ConfigSchema
+    opts = { [[ your custom config here ]] }
+  }
 }
